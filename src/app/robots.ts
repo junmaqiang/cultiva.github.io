62fa96b37bb37c29a66fc6d96ddde20324d0c659
jun <jun@Liu.local>
2026-06-09 16:01:13 +0800

    fix:更新robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,5 +7,8 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/_next/', '/static/'],
     },
+    sitemap: 'https://cultiva100.net/sitemap.xml',
   };
 }
+// 🔑 核心：加上下面这行代码！
+export const dynamic = 'force-static'
